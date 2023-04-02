7aaa18dad840a77f31f6ff0272fb256b02067257
zapod838 <45763055+zapod838@users.noreply.github.com>
2023-04-02 16:28:51 +0530

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,1 +1,5 @@
+import numpy as np # Importing NumPy library
+import pandas as pd # Importing Pandas library
+import matplotlib.pyplot as plt # Importing Matplotlib library's "pyplot" module
+import seaborn as sns # Imorting Seaborn library
 
