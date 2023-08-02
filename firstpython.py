bd32e09b144052511212b8e2080899dda08dc411
Manish Kamble <45763055+zapod838@users.noreply.github.com>
2023-08-02 23:25:46 +0530

    Delete firstpython.py

--- firstpython.py
+++
@@ -1,5 +0,0 @@
-import numpy as np # Importing NumPy library
-import pandas as pd # Importing Pandas library
-import matplotlib.pyplot as plt # Importing Matplotlib library's "pyplot" module
-import seaborn as sns # Imorting Seaborn library
-
